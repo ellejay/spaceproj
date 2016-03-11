f5d7a6c11abe73cf4df9d859970e358c89fafcd3
Laura <ellejay@users.noreply.github.com>
2016-03-11 14:33:53 +0000

    Commenting/refactoring the solar system view. Added a reset button for speed.

--- src/solarsystem/model/SolarSystemController.java
+++ src/solarsystem/model/SolarSystemController.java
@@ -19,33 +19,39 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
-import javafx.scene.input.MouseDragEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import solarsystem.objects.BodyInSpace;
 import solarsystem.objects.SpaceObjects;
- 
+
+/**
+ * Controller class used to animate the solar system view of the program.
+ * @author Laura McGhie
+ */
 public class SolarSystemController extends SuperController implements Initializable {
 
     @FXML private Pane systemPane;
     @FXML private Slider zoomSlide;
     @FXML private Button switchScene;
+	@FXML private Button resetButton;
+	@FXML private Button speedButton;
+	@FXML private Button slowButton;
 	private Timeline timeline;
 	private Map<String, BodyInSpace> selection = SpaceObjects.getPlanets();
     
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        setUp();
 
-        SCREEN_SCALE = 0.045; 
+		setUpPlanets();
+
+		// Initialise the slider to the screen scale value
 		zoomSlide.setValue(SCREEN_SCALE);
 
+		/* Give control of the screen scale to the displayed slider, and adjust the orbits if the slider is moved */
 		zoomSlide.valueProperty().addListener(new ChangeListener<Number>() {
-			public void changed(ObservableValue<? extends Number> ov,
-					Number old_val, Number new_val) {
+			public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
 				SCREEN_SCALE = (double) new_val;
 				for (BodyInSpace current: selection.values()) {
 					current.adjustGUIOrbit(current.getOrbit() * SCREEN_SCALE);
@@ -55,29 +61,34 @@ public class SolarSystemController extends SuperController implements Initializa
     }
     
     
-    private void setUp() {
-    	
+    private void setUpPlanets() {
+
+		// Redraw the planet set at 0,0 to counteract object displacement
     	for (BodyInSpace current : selection.values()) {
             current.resetPlanet();
         }
-		
+
+		/* Event handler to move the planets around their orbits. For each planet shown on the screen, we
+		 * increment their current angle by the speed factor, work out the x and y co-ords on their orbit circle
+		 * that correspond to this angle, and then move the planet to this location. */
 		EventHandler<ActionEvent> planetMovement = new EventHandler<ActionEvent>() { 
 			@Override
 			public void handle(ActionEvent event) {
 
 				for (BodyInSpace current: selection.values()) {
 
+					// Increment the angle of the planet by the speed factor
 					current.incrementAngle(SPEED_FACTOR);
 
-					// p(x) = x(0) + r * sin(a)
-					// p(y) = y(y) - r * cos(a)
-					
+					/* Work out the x & y co-ords that correspond to the new angle. Multiply by the screen scale
+					 * to ensure x & y apply to the current display ratio. */
 					double moveX = current.getParent().getX() + (current.getOrbit() * SCREEN_SCALE) * 
 							Math.sin(current.getAngle());
 					
 					double moveY = current.getParent().getY() - (current.getOrbit() * SCREEN_SCALE) * 
 							Math.cos(current.getAngle());
-					
+
+					// Update the planet position and move the planet
 					current.setPosition(moveX, moveY);
 					moveBall(current.getGUIObject(), moveX, moveY);
 				}
@@ -86,66 +97,37 @@ public class SolarSystemController extends SuperController implements Initializa
 		};
 
 
+		// Set up a timeline to display the animation
 		timeline = new Timeline(new KeyFrame(Duration.ZERO, planetMovement),
 				new KeyFrame(Duration.millis(STEP_DURATION)));
 
 		timeline.setCycleCount(Timeline.INDEFINITE);
-        //timeline.setRate(STEP_DURATION);
-		
+
+		// Add the sun at the centre of the display
 		BodyInSpace sun = SpaceObjects.getSun();
-		sun.moveGUIObject(midPoint, midPoint);
+		sun.moveGUIObject(systemPane.getPrefWidth() / 2, systemPane.getPrefHeight() / 2);
 		systemPane.getChildren().add(sun.getGUIObject());
 
+		// Set scale to end of the scale for the sun
+		SCREEN_SCALE = SpaceObjects.getScale("Sun").get(0);
+
+		// Draw the orbit at the scale indicated and add the orbit path and the planet to the screen
 		for (BodyInSpace current: selection.values()) {
 			current.adjustGUIOrbit(current.getOrbit() * SCREEN_SCALE);
 			systemPane.getChildren().add(current.getGUIOrbit());
 			systemPane.getChildren().add(current.getGUIObject());
 		}
-		
-		EventHandler<MouseEvent> moveSystem = new EventHandler<MouseEvent>() {
-			
-			double startX;
-			double startY;
-
-			@Override
-			public void handle(MouseEvent event) {
-				
-				if (event.getEventType() == MouseDragEvent.MOUSE_DRAG_ENTERED) {
-					startX = event.getX();
-					startY = event.getY();
-				}
-				else {
-					systemPane.setTranslateX(systemPane.getTranslateX() + (event.getX() - startX));
-					systemPane.setTranslateY(systemPane.getTranslateY() + (event.getY() - startY));
-				}
-				
-				event.consume();
-				
-			}
-			
-		};
-		
-		EventHandler<MouseEvent> startDrag = new EventHandler<MouseEvent>() {
-
-			@Override
-			public void handle(MouseEvent event) {
-				systemPane.startFullDrag();
-				
-			}
-			
-		};
-		
-		systemPane.addEventHandler(MouseDragEvent.DRAG_DETECTED, startDrag);
-		systemPane.addEventHandler(MouseDragEvent.MOUSE_DRAG_ENTERED, moveSystem);	
-		systemPane.addEventHandler(MouseDragEvent.MOUSE_DRAG_OVER, moveSystem);
-		systemPane.addEventHandler(MouseDragEvent.MOUSE_DRAG_EXITED, moveSystem);
-		
 
 		timeline.play();
-	
     }
 
-    
+
+    /**
+	 * Internal method to animate the movement of a body from one location to the next.
+	 * @param ball The GUI object to be moved
+	 * @param x The x position to move this object to
+	 * @param y The y position to move this object to
+     */
     private void moveBall(Circle ball, double x, double y) {
 		TranslateTransition move = new TranslateTransition(
 				Duration.millis(STEP_DURATION), ball);
@@ -153,38 +135,65 @@ public class SolarSystemController extends SuperController implements Initializa
 		move.setToY(y);
 		move.playFromStart();
 	}
-    
-    @FXML protected void slowMovement() {
-    	updateSpeed(false);
-    }
-    
-    @FXML protected void speedUpMovement() {
-    	updateSpeed(true);
-    }
-    
-    private void updateSpeed(boolean increase) {
 
-		if (increase && timeline.getRate() < 32) {
-			//timeline.setRate(timeline.getRate() * 2);
-			SPEED_FACTOR = SPEED_FACTOR * 2;
-		}
-		else if (!increase && timeline.getRate() > (1/32)){
-			//timeline.setRate(timeline.getRate() / 2);
+    /**
+	 * Decreases the current movement speed of the system, caps speed at x1/64 and disables button if at cap
+	 */
+    @FXML protected void slowMovement() {
+		if (SPEED_FACTOR > 0.015625) {
 			SPEED_FACTOR = SPEED_FACTOR / 2;
 		}
 
+		if (SPEED_FACTOR == 0.015625) {
+			slowButton.setDisable(true);
+		} else {
+			slowButton.setDisable(false);
+		}
     }
-    
-    @FXML protected void stopTimeline() throws IOException {
+
+    /**
+	 * Increases the current movement speed of the system, caps speed at x512 and disables button if at cap
+	 */
+    @FXML protected void speedUpMovement() {
+		if (SPEED_FACTOR < 512) {
+			SPEED_FACTOR = SPEED_FACTOR * 2;
+		}
+
+		if (SPEED_FACTOR == 512) {
+			speedButton.setDisable(true);
+		} else {
+			speedButton.setDisable(false);
+		}
+    }
+
+	/**
+	 * Reset the speed factor to its original value, enable both buttons
+	 */
+	@FXML protected void resetMovement() {
+		SPEED_FACTOR = 1;
+
+		speedButton.setDisable(false);
+		slowButton.setDisable(false);
+	}
+
+    /**
+	 * Method used to move from the current view to the Path Selector
+	 * @throws IOException
+     */
+    @FXML protected void mapJourney() throws IOException {
+		// Pause the current animation
     	timeline.pause();
-    	
+
+		// Use the button clicked on to get a reference to the window
     	Stage stage; 
     	Parent root;
     	stage=(Stage) switchScene.getScene().getWindow();
-    	
+
+		// Load the XML file for the Path Selector into a scene
     	root = FXMLLoader.load(getClass().getResource("/solarsystem/resources/xml/pathselect.fxml"));
     	Scene scene = new Scene(root);
-    	
+
+		// Replace the current window contents with the Path Selector
         stage.setScene(scene);
         stage.show();
     }
--- src/solarsystem/model/SuperController.java
+++ src/solarsystem/model/SuperController.java
@@ -2,9 +2,13 @@ package solarsystem.model;
 
 import java.util.ArrayList;
 
+/**
+ * Static super controller class with variables shared across multiple screens of the program.
+ * @author Laura McGhie
+ */
+
 class SuperController {
-	
-    // Planet scale factor
+
  	static double SCREEN_SCALE = 0.045;
  	static final double midPoint = 300;
  	static final double STEP_DURATION = 2; //milliseconds
